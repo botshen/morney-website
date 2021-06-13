c50cc14cdc9acf7645cb4ec712eb2aa3cb127fa6
botshen <363088847@qq.com>
2021-06-13 12:55:35 +0800

    deploy

---
+++ precache-manifest.d163fc8b3d389f5cdf7df3fc20bb4281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a8d5066dc782332bcb",
+    "url": "/morney-website/css/app.da900584.css"
+  },
+  {
+    "revision": "86da6c5aa0e7f831163956ed95cdc46e",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "e4a8d5066dc782332bcb",
+    "url": "/morney-website/js/app.6281756a.js"
+  },
+  {
+    "revision": "d85684458f40f59d3d25",
+    "url": "/morney-website/js/chunk-vendors.862e613e.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "94fb5cb990e0254359fb237989146baf",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
